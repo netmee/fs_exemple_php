d1a139cdd3cadc5845d0e16a4c4e01eefef7212b
Nicolas Krzyzanowski <nk@netmee.com>
2018-07-20 11:13:59 +0200

    Update callback.php

--- callback.php
+++ callback.php
@@ -21,12 +21,10 @@ require_once("init.php");
 $_SESSION['user_info'] = $franceConnect->callback();
 $access_token = $_SESSION['user_info']['access_token']; 
 
-$fdTest = new FDTest($franceConnect);
-$_SESSION['fd_user_info'][$fdTest->getName()] = $fdTest->getInfo($access_token);
+//$fdTest = new FDTest($franceConnect);
+//SESSION['fd_user_info'][$fdTest->getName()] = $fdTest->getInfo($access_token);
 
-$fdGuichet = new FDGuichetBreton($franceConnect);
-$_SESSION['fd_user_info'][$fdGuichet->getName()] = $fdGuichet->getInfo($access_token);
-
-
+//dGuichet = new FDGuichetBreton($franceConnect);
+//SESSION['fd_user_info'][$fdGuichet->getName()] = $fdGuichet->getInfo($access_token);
 
 header("Location: index.php");
