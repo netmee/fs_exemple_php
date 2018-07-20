bc938f70376db0df63461b4fde49606a478f156d
Nicolas Krzyzanowski <nk@netmee.com>
2018-07-20 12:07:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -42,8 +42,16 @@ Identifié en tant que <b><?php echo $_SESSION['user_info']['given_name']." ".$_
 		</tr>
 	<?php endforeach;?>
 </table>
-<h2>Données issues des fournisseurs de données </h2>
 
+<h2>Données issues du checktoken</h2>
+<table>
+	<?php foreach($_SESSION['checktoken_info'] as $key=>$value): ?>
+		<tr>
+			<th><?php echo $key ?></th>
+			<td><?php echo $value ?></td>
+		</tr>
+	<?php endforeach;?>
+</table>
 <?php else :?>
 <form action='authentication.php' method='post'>
 	<input type="submit" value="S'identifier avec France Connect" style="background:url(fc_bouton_alt1_v2.png) no-repeat; width:224px; height:56px; font-size:0; border:0; cursor:pointer;"/>
