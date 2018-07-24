c897cc342c2479af6410da4ac50d5e05c0f1ceae
Nicolas Krzyzanowski <nk@netmee.com>
2018-07-24 13:25:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,15 @@ include("init.php");
 
 <html>
 <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-8733929-16"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-8733929-16');
+</script>
 <title>Fournisseur de service France-Connect</title>
 </head>
 
